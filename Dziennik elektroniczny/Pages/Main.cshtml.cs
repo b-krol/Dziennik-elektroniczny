0829b20ace52ca2a68238fec9b95c2c2a29f2933
Bartosz Król <you@example.com>
2023-10-19 22:09:34 +0200

    Podstawowe regulacje designu i kolorów

---
+++ Dziennik elektroniczny/Pages/Main.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dziennik_elektroniczny.Pages
+{
+    public class MainModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
